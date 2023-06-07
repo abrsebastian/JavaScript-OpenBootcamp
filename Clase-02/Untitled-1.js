a40dc664156ab92845963a8122179adca5b697c3
abrsebastian <sav8421@gmail.com>
2023-06-06 23:56:49 -0300

    Tarea 2

---
+++ Clase-02/Untitled-1.js
@@ -0,0 +1,10 @@
+const nombre = "Sebastian";
+const edad = 25;
+const eresDesarrollador 
+const si = true;
+const no = false;
+if(eresDesarrollador = si){
+    console.log(Eres Desarrollador)
+}else if(eresDesarrollador = no){
+    console.log("No eres desarrollador")
+}
