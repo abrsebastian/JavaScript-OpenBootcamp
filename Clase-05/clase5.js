7a0e9ad53a487d132f6cf8d091c8981f208c7334
abrsebastian <sav8421@gmail.com>
2023-06-12 21:58:53 -0300

    completada la clase 5

---
+++ Clase-05/clase5.js
@@ -0,0 +1,13 @@
+let alturaEnCentimetrosEnteros = 174;
+let alturaEnMetros = 1.74;
+let peso = 72.2;
+
+let alturaEnMetrosRedondeado = Math.ceil(alturaEnMetros)
+console.log(alturaEnCentimetrosEnteros)
+
+let pesoRedondeadoAbajo = Math.floor(peso)
+console.log(pesoRedondeadoAbajo)
+
+let igualdad = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(igualdad)
+
