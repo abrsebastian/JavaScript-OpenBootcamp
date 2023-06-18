708e14cf73cc9f679d2dfa9b3c0a5f99ad24558d
abrsebastian <sav8421@gmail.com>
2023-06-18 19:20:33 -0300

    Finalizado el ejercicio 1/4 de la Clase 7

---
+++ Clase-07/conjuntos.js
@@ -0,0 +1,15 @@
+const familia = ["Alejandro", "Beatriz"]
+
+const familiaSet = new Set(familia)
+console.log(familiaSet)
+
+familiaSet.add("Sebastian")
+familiaSet.add("Sebastian")
+
+console.log(familiaSet)
+
+familiaSet.add("JavaScript")
+
+console.log(familiaSet)
+
+//Bienvenido a la familia JavaScript ♥
